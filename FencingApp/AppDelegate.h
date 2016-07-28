b08ea269e2bd7dcf1cd4b300fca380cb1afcb495
Aniruddha Kadam <akadam@sabaits-macbook-pro.local>
2016-07-28 22:57:22 +0530

    Initial Commit

---
+++ FencingApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FencingApp
+//
+//  Created by Aniruddha Kadam on 28/07/16.
+//  Copyright © 2016 Aniruddha Kadam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FencingApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FencingApp
+//
+//  Created by Aniruddha Kadam on 28/07/16.
+//  Copyright © 2016 Aniruddha Kadam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
